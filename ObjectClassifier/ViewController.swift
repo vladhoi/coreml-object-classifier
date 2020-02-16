6769d4b294aa5378e6e0ab1af259559becfadfb1
Vlad hoi <goyvlad2000@gmail.com>
2020-02-16 13:02:19 +0200

    Adden Constraints

--- ObjectClassifier/ViewController.swift
+++ ObjectClassifier/ViewController.swift
@@ -8,15 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var selectedImageView: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectedImageView.image = userPickedImage
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
     }
-
     
+    @IBAction func selectPhotoFromLibrary(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func takePhotoButtonPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
     
 }
 
