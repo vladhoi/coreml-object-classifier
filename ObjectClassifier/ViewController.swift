656fe9e28a2e77d0dae351a4d730b1291948aa40
Vlad hoi <goyvlad2000@gmail.com>
2020-02-16 10:30:54 +0200

    Project Init

---
+++ ObjectClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectClassifier
+//
+//  Created by Vlad Goy on 16.02.2020.
+//  Copyright © 2020 Vlad Goy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
