ea9d43898295a5406c4f9ea1b584d4cdf49175d0
Vlad hoi <goyvlad2000@gmail.com>
2020-02-16 12:19:50 +0200

    Adden ResultView

--- ObjectClassifier/ResultViewController.swift
+++ ObjectClassifier/ResultViewController.swift
@@ -12,6 +12,11 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
+        let label = UILabel()
+        label.text = "Works"
+        label.frame = CGRect(x: 0, y:0, width: 100, height: 50)
+        view.addSubview(label)
     }
     
 }
