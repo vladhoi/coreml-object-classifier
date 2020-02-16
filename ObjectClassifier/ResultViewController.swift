96be48378f3b2655c09bad5d9280e88c3be62bc1
Vlad hoi <goyvlad2000@gmail.com>
2020-02-16 12:19:33 +0200

    Adden ResultView

---
+++ ObjectClassifier/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultViewController.swift
+//  ObjectClassifier
+//
+//  Created by Vlad Goy on 16.02.2020.
+//  Copyright © 2020 Vlad Goy. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
